195318edebf43668ac5fde02674e037c35bd475d
Jhonatan <jhonatancardoso09@gmail.com>
2019-10-17 13:25:10 -0300

    react card with json

---
+++ src/componentes/Card-Info.js
@@ -0,0 +1,35 @@
+const cardInfo = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?cs=srgb&dl=adorable-animal-blur-617278.jpg&fm=jpg",
+        alt: "cat1",
+        text: "First card"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/682953/pexels-photo-682953.jpeg?cs=srgb&dl=adorable-animal-animal-photography-682953.jpg&fm=jpg",
+        alt: "cat2",
+        text: "Second card"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/416208/pexels-photo-416208.jpeg?cs=srgb&dl=adorable-animal-animal-photography-416208.jpg&fm=jpg",
+        alt: "cat3",
+        text: "Third card"
+    },
+    {
+        id: 4,
+        img: "https://images.pexels.com/photos/544502/pexels-photo-544502.jpeg?cs=srgb&dl=adorable-animal-animal-photography-544502.jpg&fm=jpg",
+        alt: "cat4",
+        text: "Fourth card"
+    },
+    {
+        id: 5,
+        img: "https://images.pexels.com/photos/2693561/pexels-photo-2693561.jpeg?cs=srgb&dl=adorable-animal-photography-animals-2693561.jpg&fm=jpg",
+        alt: "cat5",
+        text: "Fifth card"
+    }
+
+]
+
+export default cardInfo
---
+++ src/componentes/card.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+
+class Card extends React.Component{
+    render() {
+        return (
+            <div>
+                <div className="card">
+                    <div className="card-image">
+                        <figure className="image is-4by3">
+                            <img src={this.props.img} alt={this.props.alt}/>
+                        </figure>
+                    </div>
+                    <div className="card-content">
+                        <div className="content">
+                            {this.props.text}
+                            
+                            <br/>
+                            <time dateTime="2019-10-15">11:50 PM - 15 Oct 2019</time>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
